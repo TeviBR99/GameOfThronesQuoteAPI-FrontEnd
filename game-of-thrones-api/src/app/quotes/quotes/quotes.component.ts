ef70326d2002a2d0fb2a4ef2f2fbb9478e9b012e
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2023-12-14 22:30:13 +0100

    Calls on quotes  API

--- game-of-thrones-api/src/app/quotes/quotes/quotes.component.ts
+++ game-of-thrones-api/src/app/quotes/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuotesService } from '../service/quotes.service';
+import { Quote } from '../model/quote.model';
 
 @Component({
   selector: 'app-quotes',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./quotes.component.scss']
 })
 export class QuotesComponent {
+
+  quotes ?: Quote[]
+
+  constructor( private quoteService: QuotesService){}
+
+  ngOnInit(){
+    this.quoteService.getRandomQuotes().subscribe(resQuotes =>{
+      this.quotes = resQuotes;
+    })
+  }
+
+  openDialog(quoteElement: Quote){
+
+  }
+
+
+
+
+
 
 }
